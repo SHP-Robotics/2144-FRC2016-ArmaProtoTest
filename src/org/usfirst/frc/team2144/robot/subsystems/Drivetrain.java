d26359a7b7d35f9470391a2317098f7055c30d0c
Andrew Zhu <mommothazaz123@gmail.com>
2016-01-14 13:51:20 -0800

    added drivetrain functions

--- src/org/usfirst/frc/team2144/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team2144/robot/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@ package org.usfirst.frc.team2144.robot.subsystems;
 import org.usfirst.frc.team2144.robot.RobotMap;
 import org.usfirst.frc.team2144.robot.commands.GatorDrive;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -18,6 +19,7 @@ public class Drivetrain extends Subsystem {
 
 	public RobotDrive robit;
 	public Talon fLeft, fRight, bLeft, bRight;
+	public Encoder leftEnc, rightEnc;
 
 	public Drivetrain() {
 		fLeft = new Talon(RobotMap.fLeftPort);
@@ -39,6 +41,43 @@ public class Drivetrain extends Subsystem {
 			robit.tankDrive(leftStickY, rightStickY);
 		else
 			robit.tankDrive(leftStickY / 2, rightStickY / 2);
+	}
+
+	public boolean have_encoders_reached_pos(int leftPos, int rightPos) {
+		if (has_left_encoder_reached_pos(leftPos) && has_right_encoder_reached_pos(rightPos)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean has_left_encoder_reached_pos(int pos) {
+		if (Math.abs(leftEnc.get()) >= pos) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean has_right_encoder_reached_pos(int pos) {
+		if (Math.abs(rightEnc.get()) >= pos) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void reset_left_encoder() {
+		leftEnc.reset();
+	}
+
+	public void reset_right_encoder() {
+		rightEnc.reset();
+	}
+
+	public void reset_encoders() {
+		reset_left_encoder();
+		reset_right_encoder();
 	}
 
 	public void initDefaultCommand() {
